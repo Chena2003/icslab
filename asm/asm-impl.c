1a4374fbdf269c5e04fee3090fa2280e6e6ff12e
Chena <2773307597@qq.com>
2024-05-13 00:59:45 +0800

    Implement functions asm_add, asm_popcnt and asm_memcpy

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -2,19 +2,56 @@
 #include <string.h>
 
 int64_t asm_add(int64_t a, int64_t b) {
-  return a + b;
+  int64_t sum;
+  asm("leaq (%1, %2, 1), %0" // 64位数据，用q
+      : "=r"(sum)
+      : "r"(a), "r"(b)
+  );
+  return sum;
 }
 
 int asm_popcnt(uint64_t x) {
   int s = 0;
-  for (int i = 0; i < 64; i++) {
-    if ((x >> i) & 1) s++;
-  }
+  asm("movl $0, %%ecx;" 
+      "1:\t movq %1, %%rax;" // 设置标号
+      "shr %%cl, %%rax;"
+      "and $0x1, %%rax;"
+      "test %%rax, %%rax;"
+      "je 2f;" // 2f表示当前代码段后一个标号
+      "incl %0;"
+      "2:\t incl %%ecx;"
+      "cmpl $0x3f, %%ecx;"
+      "jle 1b" // 1b表示当前代码段前一个标号
+      : "+r"(s)
+      : "r"(x)
+      : "%ecx", "%rax"
+  );
+
+  // chatgpt生成版本
+  //   asm(
+  //     "movq %1, %%rax;"   // 将x加载到rax中
+  //     "xorq %%rcx, %%rcx;" // 用于计数的寄存器清零
+  //     "popcntq %%rax, %%rax;" // 对rax中的位进行计数，结果存储在rax中
+  //     "movl %%eax, %0;" // 将rax的低32位（popcntq结果）存储到s中
+  //     : "=r"(s) // 输出：s对应rax的低32位
+  //     : "r"(x) // 输入：x对应rax
+  //     : "%rax", "%rcx" // 用到的寄存器
+  // ); 
+
   return s;
 }
 
 void *asm_memcpy(void *dest, const void *src, size_t n) {
-  return memcpy(dest, src, n);
+  asm(
+      "movq %1, %%rdi;"
+      "movq %2, %%rsi;"
+      "movq %3, %%rdx;"  // n存储在64位寄存器中，故要用q
+      "call memcpy;"
+      : "+r"(dest)
+      : "r"(dest), "r"(src), "r"(n)
+  );
+
+  return dest;
 }
 
 int asm_setjmp(asm_jmp_buf env) {
--- asm/main.c
+++ asm/main.c
@@ -7,7 +7,16 @@ int main() {
   int r = asm_setjmp(buf);
   if (r == 0) {
     assert(asm_add(1234, 5678) == 6912);
+
     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
+    assert(asm_popcnt(~0ULL) == 64);
+    assert(asm_popcnt(0ULL) == 0);
+
+    char str[] = "hello world";
+    char s[128];
+    asm_memcpy(s, str, sizeof(str));
+    puts(s);
+
     // TODO: add more tests here.
     asm_longjmp(buf, 123);
   } else {
